25b6693135f97f3f72a492625e962c5cf2e43be0
Maarten Smith <s1190598@student.windesheim.nl>
2023-03-27 11:04:48 +0200

    Add ViewModels: BookDetails/ListingBook/Beheerder and ViewModelBase

---
+++ bibliotheek.WPF/ViewModels/BeheerderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace bibliotheek.WPF.ViewModels
+{
+    class BeheerderViewModel : ViewModelBase
+    {
+        public ICommand AddBookCommand { get; }
+    }
+}
