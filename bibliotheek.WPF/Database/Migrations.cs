324276c45798758bec2632b20df99b297665e554
Maarten Smith <s1190598@student.windesheim.nl>
2023-04-10 20:19:43 +0200

    Migration add but EntityFrameworkCore still dont work for me :(

---
+++ bibliotheek.WPF/Database/Migrations.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GARBAGE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "img",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_img", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "ntext", nullable: true),
+                    Summary = table.Column<string>(type: "ntext", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Tags = table.Column<string>(type: "ntext", nullable: true),
+                    Length = table.Column<int>(type: "int", nullable: true),
+                    ReleasedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_item", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Description = table.Column<string>(type: "ntext", nullable: true),
+                    Tags = table.Column<string>(type: "ntext", nullable: true),
+                    Img_idId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_author", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_author_img_Img_idId",
+                        column: x => x.Img_idId,
+                        principalTable: "img",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    Img_idId = table.Column<int>(type: "int", nullable: true),
+                    Phonenumber = table.Column<int>(type: "int", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_img_Img_idId",
+                        column: x => x.Img_idId,
+                        principalTable: "img",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorModelItemModel",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    ItemsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorModelItemModel", x => new { x.AuthorId, x.ItemsId });
+                    table.ForeignKey(
+                        name: "FK_AuthorModelItemModel_author_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "author",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorModelItemModel_item_ItemsId",
+                        column: x => x.ItemsId,
+                        principalTable: "item",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_author_Img_idId",
+                table: "author",
+                column: "Img_idId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorModelItemModel_ItemsId",
+                table: "AuthorModelItemModel",
+                column: "ItemsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Img_idId",
+                table: "Users",
+                column: "Img_idId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorModelItemModel");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "author");
+
+            migrationBuilder.DropTable(
+                name: "item");
+
+            migrationBuilder.DropTable(
+                name: "img");
+        }
+    }
+}
