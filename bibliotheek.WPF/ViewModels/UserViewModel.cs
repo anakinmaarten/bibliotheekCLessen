c7323d443cabca01ca9a380da53599f1762df211
anakinmaarten <anakinmaarten@gmail.com>
2023-04-11 11:53:51 +0200

    meer er bij xx stephen <3

---
+++ bibliotheek.WPF/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GARBAGE.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GARBAGE.ViewModels
+{
+    public partial class UserViewModel
+    {
+        
+    }
+}
